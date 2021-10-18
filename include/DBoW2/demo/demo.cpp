57edfb2b0f93a031adce2f0eff38246036711cd7
yifan <55897231+yifan-cloud@users.noreply.github.com>
2021-10-17 23:43:37 -0400

    Add files via upload

---
+++ include/DBoW2/demo/demo.cpp
@@ -0,0 +1,246 @@
+/**
+ * File: Demo.cpp
+ * Date: November 2011
+ * Author: Dorian Galvez-Lopez
+ * Description: demo application of DBoW2
+ * License: see the LICENSE.txt file
+ */
+
+#include <iostream>
+#include <vector>
+
+// DBoW2
+#include "DBoW2.h" // defines OrbVocabulary and OrbDatabase
+
+// OpenCV
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/features2d.hpp>
+
+
+using namespace DBoW2;
+using namespace std;
+using namespace cv;
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+void loadFeatures(vector<vector<cv::Mat > > &features);
+void changeStructure(const cv::Mat &plain, vector<cv::Mat> &out);
+void testVocCreation(const vector<vector<cv::Mat > > &features);
+void testDatabase(const vector<vector<cv::Mat > > &features);
+vector<cv::Mat> extracting_feature(const cv::Mat &img);
+int find_bestmatch(const vector<cv::Mat> &feature, const string &database_path);
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+// number of training images
+const int NIMAGES = 5;
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+void wait()
+{
+  cout << endl << "Press enter to continue" << endl;
+  getchar();
+}
+
+// ----------------------------------------------------------------------------
+
+int main()
+{
+  // vector<vector<cv::Mat > > features;
+  vector<cv::Mat > feature;
+  Mat greyimg;
+  greyimg = imread("./images/image6.png",IMREAD_UNCHANGED);
+
+  feature = extracting_feature(greyimg);
+  int index;
+  index = find_bestmatch(feature,"small_db.yml.gz");
+  // loadFeatures(features);
+
+  // testVocCreation(features);
+
+  // wait();
+
+  // testDatabase(features);
+
+  return 0;
+}
+
+// ----------------------------------------------------------------------------
+
+
+vector<cv::Mat> extracting_feature(const cv::Mat &img) {
+    // extract feature of the image
+    vector<cv::Mat> feature;
+    cv::Mat mask;
+    vector<cv::KeyPoint> keypoints;
+    cv::Ptr<cv::ORB> orb = cv::ORB::create();
+    cv::Mat descriptors;
+
+    orb->detectAndCompute(img, mask, keypoints, descriptors);
+    changeStructure(descriptors, feature);
+    cout << "sucessfully extract features of the image" << endl;
+    // cout << descriptors << endl;
+    return feature;
+}
+
+int find_bestmatch(const vector<cv::Mat> &feature, const string &database_path){
+    OrbDatabase db(database_path);
+    QueryResults ret;
+
+    BowVector v;
+    const int k = 9;
+    const int L = 3;
+    const WeightingType weight = TF_IDF;
+    const ScoringType scoring = L1_NORM;
+    OrbVocabulary voc(k, L, weight, scoring);
+
+    voc.transform(feature, v);
+
+
+    // cout<<feature[10]<<endl;
+    cout<<"Starting query"<<endl;
+    db.query(feature, ret);
+
+    cout<<"Best match is: "<<ret[0].Id<<endl;
+
+    return ret[0].Id;
+}
+
+void loadFeatures(vector<vector<cv::Mat > > &features)
+{
+  features.clear();
+  features.reserve(NIMAGES);
+
+  cv::Ptr<cv::ORB> orb = cv::ORB::create();
+
+  cout << "Extracting ORB features..." << endl;
+  for(int i = 0; i < NIMAGES; ++i)
+  {
+    stringstream ss;
+    ss << "images/image" << i << ".png";
+
+    cv::Mat image = cv::imread(ss.str(), 0);
+    cv::Mat mask;
+    vector<cv::KeyPoint> keypoints;
+    cv::Mat descriptors;
+
+    orb->detectAndCompute(image, mask, keypoints, descriptors);
+
+    features.push_back(vector<cv::Mat >());
+    changeStructure(descriptors, features.back());
+
+    // cout<<features.back()<<endl;
+  }
+}
+
+// ----------------------------------------------------------------------------
+
+void changeStructure(const cv::Mat &plain, vector<cv::Mat> &out)
+{
+  out.resize(plain.rows);
+
+  for(int i = 0; i < plain.rows; ++i)
+  {
+    out[i] = plain.row(i);
+  }
+  // cout<<out<<endl;
+}
+
+// ----------------------------------------------------------------------------
+
+void testVocCreation(const vector<vector<cv::Mat > > &features)
+{
+  // branching factor and depth levels 
+  const int k = 9;
+  const int L = 3;
+  const WeightingType weight = TF_IDF;
+  const ScoringType scoring = L1_NORM;
+
+  OrbVocabulary voc(k, L, weight, scoring);
+
+  cout << "Creating a small " << k << "^" << L << " vocabulary..." << endl;
+  voc.create(features);
+  cout << "... done!" << endl;
+
+  cout << "Vocabulary information: " << endl
+  << voc << endl << endl;
+
+  // lets do something with this vocabulary
+  cout << "Matching images against themselves (0 low, 1 high): " << endl;
+  BowVector v1, v2;
+  for(int i = 0; i < NIMAGES; i++)
+  {
+    voc.transform(features[i], v1);
+    for(int j = 0; j < NIMAGES; j++)
+    {
+      voc.transform(features[j], v2);
+      
+      double score = voc.score(v1, v2);
+      cout << "Image " << i << " vs Image " << j << ": " << score << endl;
+    }
+  }
+
+  // save the vocabulary to disk
+  cout << endl << "Saving vocabulary..." << endl;
+  voc.save("small_voc.yml.gz");
+  cout << "Done" << endl;
+}
+
+// ----------------------------------------------------------------------------
+
+void testDatabase(const vector<vector<cv::Mat > > &features)
+{
+  cout << "Creating a small database..." << endl;
+
+  // load the vocabulary from disk
+  OrbVocabulary voc("small_voc.yml.gz");
+  
+  OrbDatabase db(voc, false, 0); // false = do not use direct index
+  // (so ignore the last param)
+  // The direct index is useful if we want to retrieve the features that 
+  // belong to some vocabulary node.
+  // db creates a copy of the vocabulary, we may get rid of "voc" now
+
+  // add images to the database
+  for(int i = 0; i < NIMAGES; i++)
+  {
+    db.add(features[i]);
+  }
+
+  cout << "... done!" << endl;
+
+  cout << "Database information: " << endl << db << endl;
+
+  // and query the database
+  cout << "Querying the database: " << endl;
+
+  QueryResults ret;
+  for(int i = 0; i < NIMAGES; i++)
+  {
+    cout<<features[i][10]<<endl;
+    db.query(features[i], ret, 4);
+
+    // ret[0] is always the same image in this case, because we added it to the 
+    // database. ret[1] is the second best match.
+
+    cout << "Searching for Image " << i << ". " << ret[1].Id << endl;
+  }
+
+  cout << endl;
+
+  // we can save the database. The created file includes the vocabulary
+  // and the entries added
+  cout << "Saving database..." << endl;
+  db.save("small_db.yml.gz");
+  cout << "... done!" << endl;
+  
+  // once saved, we can load it again  
+  cout << "Retrieving database once again..." << endl;
+  OrbDatabase db2("small_db.yml.gz");
+  cout << "... done! This is: " << endl << db2 << endl;
+}
+
+// ----------------------------------------------------------------------------
+
+
